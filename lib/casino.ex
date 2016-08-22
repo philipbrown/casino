1bca49a1890c734bd82e4a2a256116714389463b
Philip Brown <phil@ipbrown.com>
2016-08-22 16:08:54 +0100

    Add helper functions

--- lib/casino.ex
+++ lib/casino.ex
@@ -12,4 +12,28 @@ defmodule Casino do
     opts = [strategy: :one_for_one, name: Casino.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def add_player(name, balance) do
+    Casino.Players.Server.add(name, balance)
+  end
+
+  def remove_player(id) do
+    Casino.Players.Server.remove(id)
+  end
+
+  def list_players do
+    Casino.Players.Server.list
+  end
+
+  def add_blackjack_table(count \\ 1) do
+    Casino.Games.Blackjack.Server.add_table(count)
+  end
+
+  def remove_blackjack_table do
+    Casino.Games.Blackjack.Server.remove_table
+  end
+
+  def count_blackjack_tables do
+    Casino.Games.Blackjack.Server.count_tables
+  end
 end
